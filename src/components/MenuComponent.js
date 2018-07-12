678dc90322f765c0f08721d7b8f608338d58dc40
lastnamearya <arya.jigyasu6815@gmail.com>
2018-07-12 13:46:56 +0530

    LifeCycle Methods ~ testing componentDidMount

--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -9,6 +9,11 @@ class Menu extends Component {
     this.state = {
       selectedDish: null
     }
+    console.log('Menu Component constructor is invoked');
+  }
+
+  componentDidMount() {
+    console.log('Menu Component componentDidMount is invoked');
   }
 
   onDishSelect(dish) {
@@ -41,12 +46,14 @@ class Menu extends Component {
           <Card onClick={() => this.onDishSelect(dish)}>
             <CardImg width="100%" src={dish.image} alt={dish.name} />
             <CardImgOverlay>
-              <CardTitle heading>{dish.name}</CardTitle>
+              <CardTitle heading="true">{dish.name}</CardTitle>
             </CardImgOverlay>
           </Card>
         </div>
       )
     });
+
+    console.log('Menu Component render is invoked');
 
     return (
       <div className="container">
