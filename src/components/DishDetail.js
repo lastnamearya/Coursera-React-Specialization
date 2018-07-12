6d1a9be2c0985ec17f51a0e270279f3276672c34
lastnamearya <arya.jigyasu6815@gmail.com>
2018-07-12 15:15:20 +0530

    Rendering Comments with Dish Details

--- src/components/DishDetail.js
+++ src/components/DishDetail.js
@@ -2,6 +2,16 @@ import React from 'react';
 import { Card, CardImg, CardBody, CardTitle, CardText } from 'reactstrap';
 
 const Details = ({ dish }) => {
+
+  const comments = dish.comments.map( (comment) => {
+    return (
+      <li key={comment.id}>
+        {comment.comment} <br />
+        -- {comment.author}
+      </li>
+    )
+  })
+
   return (
     <Card>
       <CardImg width="100%" src={dish.image} alt={dish.name} />
@@ -9,6 +19,12 @@ const Details = ({ dish }) => {
         <CardTitle heading="true">{dish.name}</CardTitle>
         <CardText>{dish.description}</CardText>
       </CardBody>
+      <div className="container">
+        <h5>Comments</h5>
+        <ul>
+          {comments}
+        </ul>
+      </div>
     </Card>
   )
 }
